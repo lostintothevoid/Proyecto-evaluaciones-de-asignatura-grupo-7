5ab61687fc6bc441abfba6dc44d37c32e63d4689
LeandroChamorro <127540706+LeandroChamorro@users.noreply.github.com>
2023-08-30 02:56:55 +0000

    Definición clase tipoEvaluación
    Se define los elementos que componen una clase de tipoEvaluacion utilizado para las evaluaciones

---
+++ tipoEvaluacion.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class tipoEvaluacion{
+  private double promedioNotas;
+  private String fecha;
+  private String contenido;
+  private ArrayList <String> preguntas;
+  
+}
